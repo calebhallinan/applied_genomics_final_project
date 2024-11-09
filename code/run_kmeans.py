a0c816a72e508e3e70b6857de49ebf5a92986c3e
calebhallinan <cph4mb@virginia.edu>
2024-11-09 15:57:29 -0500

    add kmeans, set pcs = 50. tried proust but need multiple images i think

--- code/run_kmeans.py
+++ code/run_kmeans.py
@@ -12,15 +12,7 @@ during_time=[1,2,3,4,5,6,7,8,9,10]
 ari_list=[1,2,3,4,5,6,7,8,9,10]
 
 new_data = sc.read_h5ad(f'{datadir}/151673.h5ad')
-def res_search_fixed_clus(adata, fixed_clus_count, increment=0.02):
 
-    for res in sorted(list(np.arange(0.2, 2.5, increment)), reverse=True):
-        sc.tl.leiden(adata, random_state=0, resolution=res)
-        count_unique_leiden = len(pd.DataFrame(adata.obs['leiden']).leiden.unique())
-        if count_unique_leiden == fixed_clus_count:
-            break
-   
-    return res
 
 #Run 10 repeated experiments
 for i in range(10):
@@ -28,22 +20,24 @@ for i in range(10):
 
     import tracemalloc
     import time
+    from sklearn.cluster import KMeans
  
     tracemalloc.start()
     start_time=time.time()
  
     adata = sc.read_h5ad(f'{datadir}/151673.h5ad')
+
     
-    
-    n_clusters=11    #according own dataset
+    n_clusters=7    #according own dataset
     sc.tl.pca(adata, n_comps=50, svd_solver='arpack')
     sc.pp.neighbors(adata, n_neighbors=20, n_pcs=50) # 20
-    eval_resolution = res_search_fixed_clus(adata, n_clusters)
-        
-    sc.tl.leiden(adata, key_added="leiden", resolution=eval_resolution)
-  
+
+    # perform kmeans clustering
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(adata.obsm["X_pca"])
+    adata.obs['kmeans'] = kmeans.labels_.astype(str)
+
     obs_df = adata.obs.dropna()
-    ari = metrics.adjusted_rand_score(obs_df['sce.layer_guess'], obs_df['leiden'])
+    ari = metrics.adjusted_rand_score(obs_df['sce.layer_guess'], obs_df['kmeans'])
     ari_list[i]=ari
 
     end_time=time.time()
@@ -61,7 +55,7 @@ for i in range(10):
     print('time: {:.4f} s'.format(during_time[i]))
     print('ARI:{}'.format(ari_list[i]))
 
-    new_data.obs['pred_{}'.format(i+1)]=adata.obs['leiden']
+    new_data.obs['pred_{}'.format(i+1)]=adata.obs['kmeans']
 
 
 # plot the results
