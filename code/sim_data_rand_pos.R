f698ab3860ef3644a1566a0b6315b1b4e8778650
Rafael dos Santos Peixoto <rafaeldossantospeixoto@gmail.com>
2024-11-11 13:33:30 -0500

    simulate data with 10 pct of cells diffused

--- code/sim_data_rand_pos.R
+++ code/sim_data_rand_pos.R
@@ -136,23 +136,110 @@ circles <- lapply(1:dim(meta)[1],
                     st_buffer(st_point(c(cell$x, cell$y)), 
                               dist = sqrt(cell$area / pi))
                   })
+names(circles) <- meta$cell
 meta <- meta %>% 
   mutate(boundary = circles)
 
+circles_sf <- st_sfc(circles) %>% st_sf()
+ggplot(data = circles_sf) +
+  geom_sf(fill = "lightblue", color = "blue") +
+  theme_minimal() +
+  labs(title = "Cell areas")
+## some cells overlap
 
 ## Simulate diffusion ----------------------------------------------------
 
+### Simulate transcript positions ----------------------------------------
+
+## percentage out of the cell
+pct_out <- .1
+## distance from the cell centroid as a function of area
+min_dist <- 1.01
+max_dist <- 2
+
+## empty matrix to get the transcripts later
+ld10_mtx <- sparseMatrix(dim(filtered_srt_mtx)[1], 
+                         dim(filtered_srt_mtx)[2], 
+                         x = 0)
+rownames(ld10_mtx) <- rownames(filtered_srt_mtx)
+colnames(ld10_mtx) <- colnames(filtered_srt_mtx)
 ## create df with transcript coordinates represented by the cell center
-transcript_list <- list()
+transcripts <- data.frame()
 ## for every cell
-for (c in 1:dim(meta)[1]) {
-  cell <- meta[i, 'cell']
-  ## get expressed transcripts
-  ts <- which(mtx[, cell] > 0)
-  for (t in 1:dim(mtx)[1]) {
-    
-    append(transcript_list, c)
+for (i in 1:dim(meta)[1]) {
+  print(paste(i, dim(meta)[1]))
+  cell <- meta[i, ]
+  cell_id <- cell$cell
+  ## get expressed transcripts 
+  ts <- filtered_srt_mtx[which(filtered_srt_mtx[, cell_id] > 0), cell_id]
+  ## calculate transcripts outside and inside the cell
+  ts_out <- round(ts * pct_out)
+  ts_in <- ts - ts_out
+  ## populate exp matrix with the transcripts inside the cell
+  for (ts_name in names(ts_in)) {
+    ld10_mtx[ts_name, cell_id] <- ts_in[ts_name]
+  }
+  ## calculate positions of the transcripts outside
+  r <- sqrt(cell$area / pi)
+  for (ts_name in names(ts_in)) {
+    ## generate random position for transcript given cell centroid
+    ## calculate angle
+    theta <- runif(1, 0, 2*pi)
+    ## calculate distance
+    d <- runif(1, r * min_dist, r * max_dist)
+    x <- cell$x + d * cos(theta)
+    y <- cell$y + d * sin(theta)
+    ## add to dataframe
+    transcripts <- rbind(transcripts, c(ts_name, x, y, cell_id))
+  }
+}
+colnames(transcripts) <- c('gene', 'x', 'y', 'orig_cell')
+transcripts <- transcripts %>% 
+  mutate(x = as.numeric(x), y = as.numeric(y))
+
+### Reassign transcripts to cells ----------------------------------------
+
+## convert transcripts to st
+sf_transcripts <- sf::st_as_sf(transcripts, coords = c('x','y'))
+
+tmp <- ld10_mtx
+## for each cell, calculate intersection
+for (cell_id in meta$cell) {
+  ## get cell geometry
+  cell_geo <- circles[[cell_id]]
+  ## intersect genes with cell
+  points_within <- st_within(sf_transcripts, cell_geo, sparse = FALSE)
+  genes_in <- sf_transcripts[points_within, ]$gene
+  if (length(genes_in) > 0) {
+    print(paste(cell_id, length(genes_in)))
+    count_genes_in <- table(genes_in)
+    for (ts_name in names(count_genes_in)) {
+      ld10_mtx[ts_name, cell_id] <- ld10_mtx[ts_name, cell_id] + 
+        count_genes_in[ts_name]
+    }
   }
 }
 
-  
+## save
+saveRDS(ld10_mtx, file = 'filtered_ld10_mtx.RDS')
+
+## check differences
+added_genes <- colSums(ld10_mtx - tmp)
+mean(added_genes) ## 2.220984
+median(added_genes) ## 1
+max(added_genes) ## 22
+
+## check original data
+mean(colSums(filtered_srt_mtx)) ## 67.43361
+median(colSums(filtered_srt_mtx)) ## 23
+max(colSums(filtered_srt_mtx)) ## 1118
+
+## check diffusion data
+mean(colSums(ld10_mtx)) ## 63.98208
+median(colSums(ld10_mtx)) ## 24
+max(colSums(ld10_mtx)) ## 1024
+
+## check data without adding genes (10 pcs loss)
+mean(colSums(tmp)) ## 61.7611
+median(colSums(tmp)) ## 22
+max(colSums(tmp)) ## 1007
